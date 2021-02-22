400dac86f63a8b128cd339724cbfe45ce63fc7a7
hallonfras <isaacwesterman@gmail.com>
2021-02-22 16:39:44 +0100

    added rudimentary command detection

--- Main.hs
+++ Main.hs
@@ -2,7 +2,7 @@
 
 import Control.Monad (when)
 import Control.Concurrent ( threadDelay )
-import Data.Text (isPrefixOf, toLower, Text)
+import Data.Text (isPrefixOf, toLower, Text, unpack)
 import qualified Data.Text.IO as TIO
 
 import UnliftIO
@@ -11,23 +11,46 @@ import Discord
 import Discord.Types
 import qualified Discord.Requests as R
 
+
+
+commands :: [(String, Message -> DiscordHandler ())]
+commands = [("zhanwei", zhanwei)]
+
+zhanwei :: Message -> DiscordHandler ()
+zhanwei m = do
+        restCall (R.CreateMessage (messageChannel m) "Yes this looks good")
+        pure ()
+
+notFound :: Message -> DiscordHandler ()
+notFound m = do
+        restCall (R.CreateMessage (messageChannel m) "That command doesnt exist!")
+        pure ()
+
+findCommand :: [(String, (Message -> DiscordHandler ()) )] -> String -> (Message -> DiscordHandler ())
+findCommand list string
+        | null list = notFound
+        | fst (head list) == string = snd (head list)
+        | otherwise = findCommand (tail list) string 
+
+
+
 -- | Replies "pong" to every message that starts with "ping"
-pingpongExample :: IO ()
-pingpongExample = do userFacingError <- runDiscord $ def
-                                            { discordToken = "#######"
-                                            , discordOnEvent = eventHandler }
-                     TIO.putStrLn userFacingError
+startBot :: IO ()
+startBot = do 
+        userFacingError <- runDiscord $ def 
+                { discordToken = "ODExOTE1MjYyNDA0MTk4NDUw.YC5JAw.Zj-bjsUJ-XdxBhWTwxOFPqfqwkU", 
+                discordOnEvent = eventHandler }
+        TIO.putStrLn userFacingError
 
 eventHandler :: Event -> DiscordHandler ()
 eventHandler event = case event of
-       MessageCreate m -> when (not (fromBot m) && isPing (messageText m)) $ do
-               _ <- restCall (R.CreateReaction (messageChannel m, messageId m) "eyes")
-               _ <- restCall (R.CreateMessage (messageChannel m) "Pong!")
-               pure ()
-       _ -> pure ()
+    MessageCreate m -> when (not (fromBot m) && isCommand (messageText m)) $ do
+        findCommand commands (tail (unpack (messageText m))) $ m
+    _ -> pure ()
 
 fromBot :: Message -> Bool
 fromBot m = userIsBot (messageAuthor m)
 
-isPing :: Text -> Bool
-isPing = ("ping" `isPrefixOf`) . toLower
+isCommand :: Text -> Bool
+isCommand text = "!" `isPrefixOf` (toLower text)
+
