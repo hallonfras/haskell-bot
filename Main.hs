cc4ade09408c38c48eb96bdb992d3a736a08f135
hallonfras <isaacwesterman@gmail.com>
2021-02-26 14:02:10 +0100

    added comments and function specifications to Main.hs

--- Main.hs
+++ Main.hs
@@ -14,30 +14,51 @@ import Discord
 import Discord.Types
 import qualified Discord.Requests as R
 
+{- main
+    loads the .env file using the Dotenv library. Then calls startbot
+-}
 main = do
         loadFile defaultConfig
         startBot
 
+-- List of commands which the bot can run
 commands :: [(String, Message -> DiscordHandler ())]
 commands = [("test", test)]
 
+{- test command
+    replies "the test function works at least" in the same discord channel where the command was sent
+    SIDE EFFECTS: performs a rest call in order to create the reply message
+-}
 test :: Message -> DiscordHandler ()
 test m = do
         restCall (R.CreateMessage (messageChannel m) "the test function works at least")
         pure ()
 
+{- test command
+    replies "That command doesnt exist!" in the same discord channel where the command was sent.
+    This is used when findCommand doesnt find an appropriate command
+    SIDE EFFECTS: performs a rest call in order to create the reply message
+-}
 notFound :: Message -> DiscordHandler ()
 notFound m = do
         restCall (R.CreateMessage (messageChannel m) "That command doesnt exist!")
         pure ()
 
+{- findCommand commandList commandName
+     finds the command with name commandName in commandList if none is found it will return the notFound function
+     RETURNS: the function which is associated with commandName or notFound if no appropriate command is found
+  -}
 findCommand :: [(String, (Message -> DiscordHandler ()) )] -> String -> (Message -> DiscordHandler ())
 findCommand list string
-        | null list = notFound
-        | fst (head list) == string = snd (head list)
-        | otherwise = findCommand (tail list) string 
+        | null list = notFound -- base case
+        | fst (head list) == string = snd (head list) -- matching command is found
+        | otherwise = findCommand (tail list) string -- inductive case
 
-
+{- startBot
+     Library function which starts the main event handling loop for the discord bot. 
+     Will print any errors it runs into to the terminal
+     SIDE EFFECTS: Prints eventual errors to the terminal
+-}
 startBot :: IO ()
 startBot = do 
         token <- getEnv "DISCORD_TOKEN"
@@ -46,15 +67,23 @@ startBot = do
                 discordOnEvent = eventHandler }
         TIO.putStrLn userFacingError
 
+{- eventHandler event
+     This function is ran by the main event handling loop once an event is received.
+     If the event was for creating a message and is a command the bot will attempt to run the command from
+     the list of available commands.
+     SIDE EFFECTS: runs a command
+-}
 eventHandler :: Event -> DiscordHandler ()
 eventHandler event = case event of
     MessageCreate m -> when (not (fromBot m) && isCommand (messageText m)) $ do
         findCommand commands (tail (unpack (messageText m))) $ m
     _ -> pure ()
 
+-- checks if the message is from a bot
 fromBot :: Message -> Bool
 fromBot m = userIsBot (messageAuthor m)
 
+-- checks if a message is a command or not. (commands are prefixed with !)
 isCommand :: Text -> Bool
 isCommand text = "!" `isPrefixOf` (toLower text)
 
